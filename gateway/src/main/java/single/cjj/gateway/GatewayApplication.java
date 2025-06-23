12d944e0a69cd43cd885e9cd5a9b31a378513d28
micor <micor.cjj@gmail.com>
2025-06-23 23:55:21 +0800

    Changes

--- gateway/src/main/java/single/cjj/gateway/GatewayApplication.java
+++ gateway/src/main/java/single/cjj/gateway/GatewayApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-        System.out.println("\u2705 Gateway \u542f\u52a8\u6210\u529f\uff01");
+        System.out.println("✅ Gateway 启动成功！");
     }
 }
