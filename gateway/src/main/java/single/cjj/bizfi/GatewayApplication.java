f33d270de80e993066e64fca8f31dfcbec52df76
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2025-06-23 22:07:45 +0800

    feat(gateway): implement spring cloud gateway module

---
+++ gateway/src/main/java/single/cjj/bizfi/GatewayApplication.java
@@ -0,0 +1,14 @@
+package single.cjj.bizfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = "single.cjj.bizfi")
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("\u2705 Gateway \u542f\u52a8\u6210\u529f!");
+    }
+}
