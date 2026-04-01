b0e90d13a63e17d50051817acca0010f017a8c09
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-04-01 16:33:26 +0800

    feat(ai): add ai conversation mapper

---
+++ base-service/src/main/java/single/cjj/bizfi/ai/mapper/BizfiAiConversationMapper.java
@@ -0,0 +1,10 @@
+package single.cjj.bizfi.ai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.bizfi.ai.entity.BizfiAiConversation;
+
+/**
+ * AI 会话 Mapper
+ */
+public interface BizfiAiConversationMapper extends BaseMapper<BizfiAiConversation> {
+}
