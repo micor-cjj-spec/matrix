116c1af11cd83d9dfc83e0dcece16b6dd4adb4b1
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:27:30 +0800

    feat(scheduler): add operation log mapper

---
+++ scheduler-service/src/main/java/single/cjj/scheduler/mapper/MatrixSchedulerOperationLogMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.scheduler.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.scheduler.entity.MatrixSchedulerOperationLog;
+
+public interface MatrixSchedulerOperationLogMapper extends BaseMapper<MatrixSchedulerOperationLog> {
+}
