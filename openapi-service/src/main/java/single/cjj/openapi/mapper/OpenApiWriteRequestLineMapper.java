087c951829bce09de891f39075ac3bfb3e902205
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:26:47 +0800

    feat: add OpenAPI write request line mapper

---
+++ openapi-service/src/main/java/single/cjj/openapi/mapper/OpenApiWriteRequestLineMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.openapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.openapi.entity.OpenApiWriteRequestLine;
+
+public interface OpenApiWriteRequestLineMapper extends BaseMapper<OpenApiWriteRequestLine> {
+}
