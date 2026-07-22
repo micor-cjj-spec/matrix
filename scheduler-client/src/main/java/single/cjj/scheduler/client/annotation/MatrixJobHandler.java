cc3459283978ba824e46f0fc7f2f6e8eb390f65a
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 17:44:29 +0800

    feat(scheduler): add handler annotation

---
+++ scheduler-client/src/main/java/single/cjj/scheduler/client/annotation/MatrixJobHandler.java
@@ -0,0 +1,15 @@
+package single.cjj.scheduler.client.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MatrixJobHandler {
+
+    String value();
+
+    String name() default "";
+}
