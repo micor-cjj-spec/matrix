40005eb5a995eecf2ef0cb298e09b0622de7f028
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:27:00 +0800

    feat: add OpenAPI write status log mapper

---
+++ openapi-service/src/main/java/single/cjj/openapi/mapper/OpenApiWriteStatusLogMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.openapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.openapi.entity.OpenApiWriteStatusLog;
+
+public interface OpenApiWriteStatusLogMapper extends BaseMapper<OpenApiWriteStatusLog> {
+}
