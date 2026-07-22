0694544818368662678dbf017d228770d5393f2f
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:28:01 +0800

    feat(scheduler): add execution entity

---
+++ scheduler-service/src/main/java/single/cjj/scheduler/entity/MatrixSchedulerExecution.java
@@ -0,0 +1,35 @@
+package single.cjj.scheduler.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("matrix_scheduler_execution")
+public class MatrixSchedulerExecution {
+
+    @TableId
+    private Long fid;
+    private String fexecutionNo;
+    private Long fjobId;
+    private String fjobCode;
+    private LocalDateTime fscheduledTime;
+    private LocalDateTime factualStartTime;
+    private LocalDateTime factualEndTime;
+    private String ftriggerType;
+    private String fstatus;
+    private Integer fattemptNo;
+    private String fexecutorCode;
+    private String fhandlerCode;
+    private String fexecutorInstance;
+    private String frequestPayload;
+    private String fresponsePayload;
+    private String ferrorCode;
+    private String ferrorMessage;
+    private String ftraceId;
+    private String fidempotencyKey;
+    private LocalDateTime fcreateTime;
+    private LocalDateTime fupdateTime;
+}
