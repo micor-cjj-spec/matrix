1318c086cb84174ae3901bc31dd834c23a2e867f
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:25:45 +0800

    feat(botp): add reconciliation issue mapper

---
+++ botp-service/src/main/java/single/cjj/botp/persistence/mapper/BotpReconciliationIssueMapper.java
@@ -0,0 +1,9 @@
+package single.cjj.botp.persistence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import single.cjj.botp.persistence.entity.BotpReconciliationIssueEntity;
+
+@Mapper
+public interface BotpReconciliationIssueMapper extends BaseMapper<BotpReconciliationIssueEntity> {
+}
