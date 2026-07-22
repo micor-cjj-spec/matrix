cb3281e1e05eb7a1d1168a53523c21aa4d0e6740
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:49:03 +0800

    feat: enable workflow outbox scheduling

--- workflow-service/src/main/java/single/cjj/workflow/WorkflowApplication.java
+++ workflow-service/src/main/java/single/cjj/workflow/WorkflowApplication.java
@@ -2,7 +2,9 @@ package single.cjj.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class WorkflowApplication {
 
