382de9870341ea8ea6b510d79adb77a0469a273b
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:26:54 +0800

    feat: add OpenAPI outbox mapper

---
+++ openapi-service/src/main/java/single/cjj/openapi/mapper/OpenApiOutboxEventMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.openapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.openapi.entity.OpenApiOutboxEvent;
+
+public interface OpenApiOutboxEventMapper extends BaseMapper<OpenApiOutboxEvent> {
+}
