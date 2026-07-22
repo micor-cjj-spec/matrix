96e67eebec16959b6461b062cdaa5eb2af15ee1e
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:38:58 +0800

    feat(fi): enable BOTP lifecycle client

--- fi-service/src/main/java/single/cjj/fi/FinanceApplication.java
+++ fi-service/src/main/java/single/cjj/fi/FinanceApplication.java
@@ -3,7 +3,9 @@ package single.cjj.fi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "single.cjj.fi.integration.botp")
 @SpringBootApplication(scanBasePackages = "single.cjj.fi")
 @MapperScan(basePackages = "single.cjj.fi.**.mapper")
 public class FinanceApplication {
