ab2a42edc5430e01f7984e79d2812063734bd270
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 17:47:15 +0800

    feat(botp): add execution target mapper

---
+++ botp-service/src/main/java/single/cjj/botp/persistence/mapper/BotpExecutionTargetMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.botp.persistence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.botp.persistence.entity.BotpExecutionTargetEntity;
+
+public interface BotpExecutionTargetMapper extends BaseMapper<BotpExecutionTargetEntity> {
+}
