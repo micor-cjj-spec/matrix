526f132c9a7b49ce44ae733d895eda0b2b97d735
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 17:46:59 +0800

    feat(botp): add rule version mapper

---
+++ botp-service/src/main/java/single/cjj/botp/persistence/mapper/BotpRuleVersionMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.botp.persistence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.botp.persistence.entity.BotpRuleVersionEntity;
+
+public interface BotpRuleVersionMapper extends BaseMapper<BotpRuleVersionEntity> {
+}
