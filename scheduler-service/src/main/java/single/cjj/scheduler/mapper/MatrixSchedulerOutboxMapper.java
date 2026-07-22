ef7dd358403a78b1767d7881c682de8ccf5659fd
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:28:41 +0800

    feat(scheduler): add outbox mapper

---
+++ scheduler-service/src/main/java/single/cjj/scheduler/mapper/MatrixSchedulerOutboxMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.scheduler.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.scheduler.entity.MatrixSchedulerOutbox;
+
+public interface MatrixSchedulerOutboxMapper extends BaseMapper<MatrixSchedulerOutbox> {
+}
