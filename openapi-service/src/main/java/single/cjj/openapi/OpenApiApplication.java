f4d8e18d573cc68ed6b90710e8605b9cd1c5983c
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:49:57 +0800

    feat: add openapi application

---
+++ openapi-service/src/main/java/single/cjj/openapi/OpenApiApplication.java
@@ -0,0 +1,17 @@
+package single.cjj.openapi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@MapperScan("single.cjj.openapi.mapper")
+@SpringBootApplication
+public class OpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiApplication.class, args);
+        System.out.println("✅ OpenAPI Service 启动成功！");
+    }
+}
