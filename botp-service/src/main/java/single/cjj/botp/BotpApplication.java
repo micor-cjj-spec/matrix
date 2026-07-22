d8ee4bd6dca723affef6d91e44c3d2f3bda6dea6
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 18:33:18 +0800

    feat(botp): enable recovery schedulers

--- botp-service/src/main/java/single/cjj/botp/BotpApplication.java
+++ botp-service/src/main/java/single/cjj/botp/BotpApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableFeignClients(basePackages = "single.cjj.botp.integration.fi")
 @MapperScan("single.cjj.botp.persistence.mapper")
 @SpringBootApplication(scanBasePackages = {"single.cjj.botp", "single.cjj.bizfi"})
