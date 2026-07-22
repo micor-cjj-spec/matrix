581ecd7da37a1ed75360ff55a5537c1d08051486
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:51:19 +0800

    feat: add openapi definition mapper

---
+++ openapi-service/src/main/java/single/cjj/openapi/mapper/OpenApiDefinitionMapper.java
@@ -0,0 +1,9 @@
+package single.cjj.openapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import single.cjj.openapi.entity.OpenApiDefinition;
+
+@Mapper
+public interface OpenApiDefinitionMapper extends BaseMapper<OpenApiDefinition> {
+}
