4c690e939201b045aab74f58318968b27b9db8a2
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-22 16:28:22 +0800

    feat(scheduler): add scheduler mappers

---
+++ scheduler-service/src/main/java/single/cjj/scheduler/mapper/MatrixSchedulerJobMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.scheduler.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.scheduler.entity.MatrixSchedulerJob;
+
+public interface MatrixSchedulerJobMapper extends BaseMapper<MatrixSchedulerJob> {
+}
