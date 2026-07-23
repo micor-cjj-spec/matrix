f71f7a711f2a6b47bd26715a3b501359085c3888
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-07-23 15:30:07 +0800

    feat(shared): enable durable task snapshot scheduling

--- share-service/src/main/java/single/cjj/share/ShareServiceApplication.java
+++ share-service/src/main/java/single/cjj/share/ShareServiceApplication.java
@@ -2,7 +2,9 @@ package single.cjj.share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ShareServiceApplication {
 
