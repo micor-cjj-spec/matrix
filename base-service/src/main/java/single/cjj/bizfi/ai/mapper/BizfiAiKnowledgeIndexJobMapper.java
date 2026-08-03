7fb84eced2b0783dc70a43ca652d8fda60b4d38d
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-08-03 21:54:47 +0800

    feat(knowledge): add index job mapper

---
+++ base-service/src/main/java/single/cjj/bizfi/ai/mapper/BizfiAiKnowledgeIndexJobMapper.java
@@ -0,0 +1,7 @@
+package single.cjj.bizfi.ai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import single.cjj.bizfi.ai.entity.BizfiAiKnowledgeIndexJob;
+
+public interface BizfiAiKnowledgeIndexJobMapper extends BaseMapper<BizfiAiKnowledgeIndexJob> {
+}
