c03449a69af34300ce9a890f500a5e010ba420b8
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-08-27 16:00:44 +0800

    feat(procurement): add sourcing SourcingAwardMapper.java

---
+++ erp-service/src/main/java/single/cjj/erp/procurement/sourcing/mapper/SourcingAwardMapper.java
@@ -0,0 +1,4 @@
+package single.cjj.erp.procurement.sourcing.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper; import org.apache.ibatis.annotations.Mapper;
+import single.cjj.erp.procurement.sourcing.entity.SourcingAwardEntity;
+@Mapper public interface SourcingAwardMapper extends BaseMapper<SourcingAwardEntity> {}
