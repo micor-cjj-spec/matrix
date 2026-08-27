1a911d2280867e7713feec5e23771ffa9005ac13
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-08-27 17:48:41 +0800

    feat(base): add business partner role mapper

---
+++ base-service/src/main/java/single/cjj/bizfi/partner/mapper/BusinessPartnerRoleMapper.java
@@ -0,0 +1,9 @@
+package single.cjj.bizfi.partner.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import single.cjj.bizfi.partner.entity.BusinessPartnerRoleEntity;
+
+@Mapper
+public interface BusinessPartnerRoleMapper extends BaseMapper<BusinessPartnerRoleEntity> {
+}
