f05a260e22a02ed0bc78956acba5d40eb724ee85
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-08-27 18:09:47 +0800

    feat(sales): add quotation mapper

---
+++ erp-service/src/main/java/single/cjj/erp/sales/quotation/mapper/SalesQuotationMapper.java
@@ -0,0 +1,24 @@
+package single.cjj.erp.sales.quotation.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import single.cjj.erp.sales.quotation.entity.SalesQuotationEntity;
+
+@Mapper
+public interface SalesQuotationMapper extends BaseMapper<SalesQuotationEntity> {
+
+    @Select("""
+            SELECT *
+              FROM matrix_erp_sales_quotation
+             WHERE fid = #{fid}
+               AND ftenant_id = #{tenantId}
+               AND fdelete_flag = 0
+             FOR UPDATE
+            """)
+    SalesQuotationEntity selectByIdForUpdate(
+            @Param("fid") Long fid,
+            @Param("tenantId") String tenantId
+    );
+}
